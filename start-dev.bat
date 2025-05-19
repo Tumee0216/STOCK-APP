40151635ba479cdbdfe300f8fd6782f3d95931dc
Tumee TMR <tumee9923TLG@gmail.com>
2025-05-19 11:20:24 +0800

    HHE

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,13 @@
 @echo off
 echo Starting the Next.js server...
+
+REM Install dependencies
 npm install
+
+REM Open the default browser to localhost:3000
+start http://localhost:3000
+
+REM Start the Next.js development server
 npm run dev
+
+pause
